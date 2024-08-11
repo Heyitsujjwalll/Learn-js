6eabafa982a3c5fe7a453432971db718a359cedd
Ujjwal Mandal <99236124+Heyitsujjwalll@users.noreply.github.com>
2024-08-11 10:27:35 +0000

    change

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,5 +12,9 @@ let accountState;
 // accountId =2   * It is not allowed because we can't change the value of constant
 
 console.log(accountEmail); 
+/*
+    prefer not to use var
+    because of issue in block scope and funtional scope
+*/
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
