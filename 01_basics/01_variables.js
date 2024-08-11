818b6577e35d2525ee48c806a7ec02c31289a9a3
Ujjwal Mandal <99236124+Heyitsujjwalll@users.noreply.github.com>
2024-08-11 10:26:13 +0000

    01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14453
+let accountEmail="ujjwal@aph.com"
+var accountPassword = "121212"
+accountCity = "jaipur"
+
+accountEmail ="ujj@uj.com"
+accountPassword = "03030303"
+accountCity = "Bhopal"
+let accountState;
+
+
+// accountId =2   * It is not allowed because we can't change the value of constant
+
+console.log(accountEmail); 
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
