77fbc66e67e148e54fdf23997708baf165391bd3
Ujjwal Mandal <99236124+Heyitsujjwalll@users.noreply.github.com>
2024-08-11 11:02:29 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+" use strict" ; //treat all js code as newer version
+
+// alert (3 + 3) * we are using node js not a browser
+
+// console.log(3
+//      +3)     * Code readebility shoulde be high
+
+console.log("ujjwal");
+let name="ujjwal"
+let age =12
+let isLoggedin=True
+
+// DATA TYPES
+/*
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => undefined
+    undefined =>
+    symbol => object
+
+    object
+    
+*/
+
+console.log(typeof "ujjwal");
+
