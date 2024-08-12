1b821d445a48c92479a9be9c46c4260bceca5628
Ujjwal Mandal <99236124+Heyitsujjwalll@users.noreply.github.com>
2024-08-12 10:36:06 +0000

    chh

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// BASIC CONVERSION
+console.log(2>1);
+console.log(2 >=1);
+
+
+// console.log("2" >1);
+// console.log("02" >1);
+
+
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >=0); //here the null is converted to the zero
+
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined <0);
+
+ // STRICT CHECK ===
+
+ console.log("2" === 2);
+ 
+
+
+
+
+
