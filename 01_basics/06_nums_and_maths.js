9cb6811d83ec9d965b993a106930e4f8aa625914
Ujjwal Mandal <99236124+Heyitsujjwalll@users.noreply.github.com>
2024-08-13 11:06:24 +0000

    nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,63 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// toString converts the numberinto a string 
+// console.log(balance.toString());
+
+// tFixed is used to give you the fixed value 
+// console.log(balance.toFixed(1));
+
+const otherNumber = 1123.8966
+
+//toPrecision givesyou the precised value af any number 
+// console.log(otherNumber.toPrecision(3));
+
+//
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+// **************** MATHS ***********
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4,5,8,9));
+// console.log(Math.max(4,5,2,0));
+
+console.log(Math.random());
+
+//console.log(Math.random()*10);
+// console.log(math.random()*10 +1);
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
